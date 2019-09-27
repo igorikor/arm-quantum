d48de80c65bacd68b5535a283e7432d22e738651
igorikor <i.shirokih@mail.ru>
2019-09-28 00:55:33 +0500

    working on the log

--- Web Test/templates/main.js
+++ Web Test/templates/main.js
@@ -1,14 +1,20 @@
 $(function(){
 
-	ip = '127.0.0.1:5000'
+	var ip = '127.0.0.1:5000'
+	var log_update = '1'
+	var lib = {
+		'1':"Turn On"
+	}
 
 	$("#connect").click(function(){
-		var com = $("#com").val()
-		$.post('http://'+ip+'/connect',{'Port':com})
+		var com = $("#com").val();
+		$.post('http://'+ip+'/connect',{'Port':com});
 	})
 
 	$("#cmd_send").click(function(){
-		var cmd = $("#cmd_text").val()
-		$.post('http://'+ip+'/',{'cmd':cmd})
+		var cmd = $("#cmd_text").val();
+		var log_update = lib[cmd];
+		$.post('http://'+ip+'/',{'cmd':cmd});
 	})
+
 })
